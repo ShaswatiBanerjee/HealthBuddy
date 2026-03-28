eb84f0e25d7c60eb76cda60259d47bde281a6aab
ShaswatiBanerjee <shaswati07banerjee@gmail.com>
2026-03-28 13:16:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,8 +101,12 @@ def logout():
 
 
 
+import os
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
